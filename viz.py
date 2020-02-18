e6e0d9d3b091150468f90fcef8d01e1b24b80a0f
mmagnuski <mmagnuski@swps.edu.pl>
2020-02-18 20:02:33 +0100

    ENH: better figure 5: additional 01 results

--- viz.py
+++ viz.py
@@ -303,36 +303,31 @@ def create_swarm_df(psd_high, psd_low):
 
 def plot_heatmap_add1(clst):
     '''Plot results of Standardized Analyses (ADD1) with heatmap and topo.'''
-    fig = plt.figure(figsize=(10, 10))
-    gs = fig.add_gridspec(2, 2, hspace=0.5, top=0.95, bottom=0.05, left=0.07)
+    fig = plt.figure(figsize=(7, 9))
+    gs = fig.add_gridspec(2, 2, hspace=0.55, top=0.95, bottom=0.05, left=0.08,
+                          height_ratios=[0.6, 0.4])
     f_ax1 = fig.add_subplot(gs[0, :])
     f_ax2 = fig.add_subplot(gs[1, 0])
     f_ax3 = fig.add_subplot(gs[1, 1])
 
     clst_idx = [0, 1] if len(clst) > 1 else None
     clst.plot(dims=['chan', 'freq'], cluster_idx=clst_idx, axis=f_ax1,
-              vmin=-4, vmax=4)
-    f_ax1.set_xlabel('Frequency (Hz)', fontsize=18)
-    f_ax1.set_ylabel('frontal channels', fontsize=18)
+              vmin=-4, vmax=4, alpha=0.65)
+    f_ax1.set_xlabel('Frequency (Hz)', fontsize=22)
+    f_ax1.set_ylabel('frontal channels', fontsize=22)
 
     contrast = clst.description['contrast']
     cbar_label = ('Regression t value' if 'reg' in contrast
                   else 't value')
     cbar_ax = fig.axes[-1]
-    cbar_ax.set_ylabel(cbar_label, fontsize=16)
+    cbar_ax.set_ylabel(cbar_label, fontsize=20)
 
     # change ticklabels fontsize
     for tck in f_ax1.get_xticklabels():
-        tck.set_fontsize(16)
+        tck.set_fontsize(18)
 
     for tck in cbar_ax.get_yticklabels():
-        tck.set_fontsize(15)
-
-    # FIXME: change axis position (doesn't work...)
-    # fig.canvas.draw()
-    # bounds = cbar_ax.get_position().bounds
-    # bounds = (0.86, *bounds[1:])
-    # cbar_ax.set_position(bounds)
+        tck.set_fontsize(16)
 
     freqs1, freqs2 = (9, 10), (11.5, 12.50)
     freqlabel1, freqlabel2 = '9 - 10 Hz', '11.5 - 12.5 Hz'
@@ -344,15 +339,15 @@ def plot_heatmap_add1(clst):
         idx1, idx2 = 1, 0
 
     # topo 1
-    mark_kwargs = {'markersize': 10}
+    mark_kwargs = {'markersize': 8}
     topo_args = dict(vmin=-4, vmax=4, mark_clst_prop=0.3,
                      mark_kwargs=mark_kwargs, border='mean')
     tp1 = clst.plot(cluster_idx=idx1, freq=freqs1, axes=f_ax2, **topo_args)
-    tp1.axes.set_title(freqlabel1, fontsize=16)
+    tp1.axes.set_title(freqlabel1, fontsize=18)
 
     # topo 2
     tp2 = clst.plot(cluster_idx=idx2, freq=freqs2, axes=f_ax3, **topo_args)
-    tp2.axes.set_title(freqlabel2, fontsize=16)
+    tp2.axes.set_title(freqlabel2, fontsize=18)
 
     obj_dict = {'heatmap': f_ax1, 'colorbar': cbar_ax, 'topo1': tp1,
                 'topo2': tp2}
